2b0d5760995648ef4c43f547756cc519c94afefb
Luca Capra <luca.capra@gmail.com>
2020-07-22 18:21:49 +0200

    fix req

--- hook/http.go
+++ hook/http.go
@@ -1,13 +1,14 @@
 package hook
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
-	"bytes"
-	"io/ioutil"
 
 	"github.com/muka/camd/device"
 	"github.com/spf13/viper"
@@ -15,10 +16,9 @@ import (
 
 //CameraSource a json payload
 type CameraSource struct {
-	Live bool `json:"live"`
-	URI string `json:"uri"`
+	Live bool   `json:"live"`
+	URI  string `json:"uri"`
 	Type string `json:"type"`
-
 }
 
 // Request Perform an HTTP request based on the event
@@ -38,9 +38,14 @@ func Request(ev device.OnChangeEvent) error {
 	if ev.Event == device.DeviceAdded {
 		method = "PUT"
 
+		uri := ev.Device.MediaURI
+		if uri == "" {
+			uri = ev.Device.Path
+		}
+
 		source := CameraSource{
 			Type: "video",
-			URI: ev.Device.MediaURI,
+			URI:  uri,
 			Live: true,
 		}
 
@@ -52,38 +57,38 @@ func Request(ev device.OnChangeEvent) error {
 		body = bytes.NewReader(b)
 	}
 
-   client := &http.Client{}
+	client := &http.Client{}
 
-    // Create request
-    req, err := http.NewRequest(method, url, body)
-    if err != nil {
-        return err
-    }
+	// Create request
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
 
 	req.Header.Add("content-type", contentType)
 
-    // Fetch Request
-    resp, err := client.Do(req)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
+	// Fetch Request
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    // Read Response Body
-    responseBody, err = ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
+	// Read Response Body
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 
 	if resp.StatusCode >= 400 {
 		return fmt.Errorf("Request failed: %d %s", resp.StatusCode, resp.Status)
 	}
 
-    if err != nil {
-        return err
+	if err != nil {
+		return err
 	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
 	log.Printf("Response %s", responseBody)
 
